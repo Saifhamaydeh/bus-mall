0592ea7a48e166d1e4247ed33a725e0376cfd182
saif hamaydeh <saif.ahmad12395@gmail.com>
2021-03-15 19:33:21 +0200

    add chart to the page

--- js/app.js
+++ js/app.js
@@ -50,15 +50,56 @@ function render()
 }
 imgSection.addEventListener('click',ch)
 function ch(event){
-    if(event.target.id === 'imgSection' )
+    event.preventDefault();
+    console.log(proMall.allObj) ;
+    if(event.target.id !== 'imgSection' )
     {
-        for(let i=0; proMall.allObj.length;i++){
+        for(let i=0; i<proMall.allObj.length;i++){
             if(proMall.allObj[i].name===event.target.title){
                 proMall.allObj[i].vote++;
+                console.table(proMall.allObj[i]);
             }
         }
         render();
+    
     }
 }
 
 render();
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],// product name
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],//number of votes for every product
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
